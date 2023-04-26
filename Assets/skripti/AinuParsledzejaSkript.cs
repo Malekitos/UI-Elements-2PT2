566e6e7da4cc1b5d57adf8aa49c39f034d4903f9
Dzonatans <Dzonatan2017@gmail.com>
2023-04-26 12:02:10 +0300

    ainas

---
+++ Assets/skripti/AinuParsledzejaSkript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AinuParsledzejaSkript : MonoBehaviour {
+
+	public void uzSakumu() {
+		SceneManager.LoadScene("Sakums",LoadSceneMode.Single);
+	}
+
+    public void apturet() {
+        Application.Quit();
+    }
+
+    public void uzScene()
+    {
+        SceneManager.LoadScene("scene", LoadSceneMode.Single);
+    }
+}
---
+++ Assets/skripti/kursorsUzAttela.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kursorsUzAttela : MonoBehaviour {
+
+	public AudioSource skanasAvots;
+	public AudioClip skanaKoAtskanet;
+
+	public void uzbiditsUzAttela() {
+		skanasAvots.PlayOneShot(skanaKoAtskanet);
+	}
+
+	public void kursorsNost() {
+		skanasAvots.Stop();
+	}
+}
